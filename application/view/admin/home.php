7a152e8542873084ac2c4525ff2ccab70a2e3039
meetcd <meetcd@a89ce20e-c831-11dd-8772-d1b041bf0e37>
2008-12-13 06:31:11 +0000

    增加后台界面

---
+++ application/view/admin/home.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>home</title>
+<link href="<?=site_path("images/admin/admin.css")?>" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div class="head">
+<div class="log"></div>
+<div class="copyright">
+Copyright 2008<br />sf version 0.1.0
+</div>
+<div class="navbar">
+<menu>
+<li class="active"><a href="#">Home</a></li>
+<li><a href="#">Function</a></li>
+<li><a href="#">Category</a></li>
+<li><a href="#">Setting</a></li>
+<li><a href="#">User Manager</a></li>
+<li><a href="#">Template Manager</a></li>
+<li><a href="#">Plug_In's</a></li>
+<li><a href="#">To-DO</a></li>
+</menu>
+</div>
+</div>
+<div class="body">
+<div class="body_left">
+<div class="body_left_functions">
+<h1>Admin Functions</h1>
+<ul>
+<li><a href="#">Category</a></li>
+<li><a href="#">Setting</a></li>
+</ul>
+</div>
+<div class="body_left_links">
+<h1>Quick Links</h1>
+<ul>
+<li><a href="#">Category</a></li>
+<li><a href="#">Setting</a></li>
+</ul>
+</div>
+</div>
+<div class="body_right">
+<div>
+<div class="body_right_tip">
+<img src="<?=site_path("images/admin/home.gif")?>" />
+<h1>Welcome!</h1>
+<p>You have been logged in!</p>
+<p style="clear:both"></p>
+</div>
+<div class="body_right_tool">
+<img src="<?=site_path("images/admin/dir.gif")?>" />
+<h1>Category Manager</h1>
+<p>The Directory Manager enables you to create and manage your directory.</p>
+<p style="clear:both"></p>
+</div>
+
+<div class="body_right_tool">
+<img src="<?=site_path("images/admin/users.gif")?>" />
+<h1>User Manager</h1>
+<p>Manage User Accounts. Note that the user manager can only be used if the user registration system is in use. If the user registration system is not in use then users don't have to register accounts in order to submit links.</p>
+<p style="clear:both"></p>
+</div>
+
+<div class="body_right_tool">
+<img src="<?=site_path("images/admin/template.gif")?>" />
+<h1>Template Manager</h1>
+<p>The template manager enables you to edit the look and feel of your Search Engine & Directory.</p>
+<p style="clear:both"></p>
+</div>
+
+</div>
+</div>
+<div style="clear:both;"></div>
+</div>
+<div class="footer"></div>
+</body>
+</html>
