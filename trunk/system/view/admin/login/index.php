10aceea40b317cff1914fc85203fdce1097e30e2
meetcd <meetcd@a89ce20e-c831-11dd-8772-d1b041bf0e37>
2009-05-05 08:55:51 +0000

    修改了登陆界面

--- trunk/system/view/admin/login/index.php
+++ trunk/system/view/admin/login/index.php
@@ -1,36 +1,60 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
-<title>¹ÜÀíÔ±µÇÂ¼</title>
-</head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>ç®¡ç†å‘˜ç™»å½•</title>
+<style type="text/css">
+<!--
+body {
+	background-image: url(<?=site_path("images/admin/login22_r1_c1.jpg")?>);
+	margin-top: 0px;
+}
+.STYLE1 {font-size: 12px}
+-->
+</style></head>
 
 <body>
-<table width="100%" height="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+<table width="1003" border="0" align="center" cellpadding="0" cellspacing="0">
   <tr>
-    <td width="1003" height="40"><table width="372" border="0" align="center" cellpadding="3" cellspacing="1" class="tb_data">
-      <form id="form1" name="form1" method="post" action="">
-        <tr>
-          <td height="36"><span class="STYLE1">ÓÃ&nbsp;&nbsp;»§£º&nbsp;&nbsp;</span>
-              <input name="username" type="text" id="username" style="width:200px; height:16px; background-color:#292929; border:solid 1px #7dbad7; font-size:12px; color:#6cd0ff" /></td>
-        </tr>
-        <tr>
-          <td height="23"><span class="STYLE1">ÃÜ &nbsp;Âë£º&nbsp;&nbsp;</span>
-              <input name="password" type="password" id="password" style="width:200px; height:16px; background-color:#292929; border:solid 1px #7dbad7; font-size:12px; color:#6cd0ff" /></td>
-        </tr>
-        <tr>
-          <td height="23"><span class="STYLE1">ÑéÖ¤Âë£º&nbsp;&nbsp;</span>
-              <input name="SafetyCode" type="text" id="SafetyCode" style="width:100px; height:16px; background-color:#292929; border:solid 1px #7dbad7; font-size:12px; color:#6cd0ff" />
-              <img src="<?=site_url("common/index")?>" align="absmiddle" /></td>
-        </tr>
-        <tr>
-          <td align="center"><label>
-            <input type="submit" name="Submit" value=" µÇ Â½ " /> 
-            <input type="reset" name="Submit2" value=" ÖØ ÖÃ " />
-          </label></td>
-        </tr>
+    <td height="40" colspan="3">&nbsp;</td>
+  </tr>
+  <tr>
+    <td colspan="3"><img src="<?=site_path("images/admin/login22_r2_c1.jpg")?>" width="1003" height="57" /></td>
+  </tr>
+  <tr>
+    <td colspan="3"><img src="<?=site_path("images/admin/login22_r3_c1.jpg")?>" width="1003" height="49" /></td>
+  </tr>
+  <tr>
+    <td colspan="3"><img src="<?=site_path("images/admin/login22_r4_c1.jpg")?>" width="1003" height="83" /></td>
+  </tr>
+  <tr>
+    <td width="298"><img src="<?=site_path("images/admin/login22_r5_c1.jpg")?>" width="298" height="125" /></td>
+    <td width="402" valign="top" background="<?=site_path("images/admin/login22_r5_c2.jpg")?>"><table width="75%" border="0" align="center" cellpadding="0" cellspacing="0">
+    <form id="form1" name="form1" method="post" action="">
+      <tr>
+        <td height="36"><span class="STYLE1">ç”¨&nbsp;&nbsp;æˆ·ï¼š&nbsp;&nbsp;</span>
+          <input name="username" type="text" id="username" style="width:200px; height:16px; background-color:#292929; border:solid 1px #7dbad7; font-size:12px; color:#6cd0ff"></td>
+      </tr>
+      <tr>
+        <td height="23"><span class="STYLE1">å¯† &nbsp;ç ï¼š&nbsp;&nbsp;</span>
+          <input name="password" type="password" id="password" style="width:200px; height:16px; background-color:#292929; border:solid 1px #7dbad7; font-size:12px; color:#6cd0ff"></td>
+        
+      </tr>
+      <tr>
+        <td height="23"><span class="STYLE1">éªŒè¯ç ï¼š&nbsp;&nbsp;</span> <input name="SafetyCode" type="text" id="SafetyCode" style="width:100px; height:16px; background-color:#292929; border:solid 1px #7dbad7; font-size:12px; color:#6cd0ff" /> <img src="<?=site_url("common/index")?>" align="absmiddle" /></td>
+      </tr>
+      <tr>
+        <td><div align="center"><input type="image" src="<?=site_path("images/admin/dl.gif")?>" width="49" height="18" />&nbsp; &nbsp;&nbsp;<img src="<?=site_path("images/admin/cx.gif")?>" width="49" height="18" /></div></td>
+      </tr>
       </form>
     </table></td>
+    <td width="303"><img src="<?=site_path("images/admin/login22_r5_c3.jpg")?>" width="303" height="125" /></td>
+  </tr>
+  <tr>
+    <td colspan="3"><img src="<?=site_path("images/admin/login22_r6_c1.jpg")?>" width="1003" height="98" /></td>
+  </tr>
+  <tr>
+    <td colspan="3"><div align="center"></div></td>
   </tr>
 </table>
 </body>
