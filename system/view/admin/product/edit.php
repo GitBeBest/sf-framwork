81f615915cbd464fe2629ab7279ba3afff6dc99a
meetcd <meetcd@a89ce20e-c831-11dd-8772-d1b041bf0e37>
2009-05-12 06:46:49 +0000

    修改了后台上传图片的方式

--- system/view/admin/product/edit.php
+++ system/view/admin/product/edit.php
@@ -1,3 +1,9 @@
+<script language="javascript" type="text/javascript"> 
+function addPic(json)
+{
+	$('<li><img src="<?=site_path("up_files/")?>/' + json[0].path + '" width="150" height="100" alt="" /><input name="images[]" type="checkbox" checked="checked" value="'+json[0].path+'" /></li>').appendTo(".img_list");
+}
+</script>
 <div class="main">
   <div class="tools"> <a href="<?=site_url("admin/product/index/type/".$type)?>" class="back">产品列表</a> <a href="#" onclick="$('#validateForm').submit();" class="save">保存资料</a> </div>
   <div class="box">
@@ -34,16 +40,23 @@
             是否发布 </label></td>
         </tr>
 		<tr>
-          <th align="right" width="80">展示图片</th>
-          <td colspan="3">
-		  <img src="<?=site_path("up_files/".$product->getCover())?>" id="img_cover" width="120" height="88" onerror="this.src='<?=site_path("images/admin/nopic.png")?>'"  class="img_upload" alt="点击上传封面图片" onclick="loadbox.load('<?=site_url("common/upload")?>',function(json){$('#cover').val(json[0].path);$('#img_cover').attr('src','<?=site_path("up_files")?>'+'/'+json[0].path);});"/>
-		  <input name="cover" type="hidden" id="cover" value="<?=$product->getCover()?>" class="required"/>
-		  <?php for($i=1;$i<6;$i++):?>
-		  <img src="<?=site_path("up_files/".$product->getImages($i))?>" width="120" height="88" id="img<?=$i?>" class="img_upload" alt="点击上传" onclick="loadbox.load('<?=site_url("common/upload")?>',function(json){$('#images<?=$i?>').val(json[0].path);$('#img<?=$i?>').attr('src','<?=site_path("up_files")?>'+'/'+json[0].path);});" onerror="this.src='<?=site_path("images/admin/nopic.png")?>'"/>
-		  <input name="images[]" type="hidden" id="images<?=$i?>" value="<?=$product->getImages($i)?>" />
-		  <?php endfor;?>
-		  </td>
+          <th width="80" rowspan="2" align="right">展示图片</th>
+          <td colspan="3"><label>
+            <input type="button" name="Submit" value=" 增加展示图片 "  onclick="loadbox.load('<?=site_url("common/upload")?>',addPic);" />
+          </label>
+          (如果想删除上传的图片，请不要勾选该图片，保存即可。)</td>
         </tr>
+		<tr>
+		  <td colspan="3"><ul class="img_list">
+		  <?php
+		  $images = $product->getImages();
+		  for($i=0,$n=count($images);$i<$n ;$i++):
+		  if($images[$i] == '') continue;
+		  ?>
+		  <li><img src="<?=site_path("up_files/".$images[$i])?>" width="150" height="100" alt="" /><input name="images[]" type="checkbox" checked="checked" value="<?=$images[$i]?>" /></li>
+		  <?php endfor;?>
+		  </ul></td>
+	    </tr>
         <tr>
           <th align="center">详细描述</th>
           <td colspan="3" align="center"><?=sf::getPlugin("Fckeditor","content",$product->getContent(),'100%',600)->create()?></td>
